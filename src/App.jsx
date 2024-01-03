bffb2a9a2ec2ccb2ca99101252e97227c5b5c50c
Joachim Mwengi <kimwambua96@gmail.com>
2024-01-03 12:59:48 +0300

    more navbar improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
