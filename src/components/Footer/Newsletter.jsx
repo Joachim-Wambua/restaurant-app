7d723814ddcbe14d0337dc06960bd91188fe8136
Joachim Mwengi <kimwambua96@gmail.com>
2024-01-06 23:32:28 +0300

    completed footer and contact sections

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import "./Newsletter.css";
+import SubHeading from "../SubHeading/SubHeading";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">Get the latest updates from us</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter Your Email Address" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
+
+import { images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">
+          1111 North Orleans Street Chicago IL 60654 United States
+        </p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="" />
+        <p className="p__opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others."
+        </p>
+        <img
+          src={images.spoon}
+          alt="Spoon"
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">
+          Monday - Friday
+        </p>
+        <p className="p__opensans">08:00 AM - 2:00 AM</p>
+        <p className="p__opensans">
+          Saturday - Sunday
+        </p>
+        <p className="p__opensans">10:00 AM - 11:00 PM</p>
+      </div>
+    </div>
+
+    <div className="footer_copyright">
+      <p className="p__opensans">2024 The Bear. All Rights Reserved.</p>
+    </div>
   </div>
 );
 
